8c8ee00a18e58f2637b19da95928cb1ff6047203
jomoseikon <seikon@jomoevent.com>
2024-10-08 10:37:17 +0900

    removed yarn.lock

--- src/BlogRoll.jsx
+++ src/BlogRoll.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState, useEffect  } from 'react';
 import Airtable from 'airtable';
+import 'bulma/css/bulma.min.css';
+import { Columns } from 'react-bulma-components';
+
 const base = new Airtable({ apiKey: process.env.VITE_APP_AIRTABLE_ACCESS_TOKEN }).base(process.env.VITE_APP_AIRTABLE_BASE_ID);
 
 import "./BlogRoll.css";
@@ -8,25 +11,23 @@ export default function BlogRoll() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        base('Schedule').select({ view: 'Grid view' }).eachPage((records, fetchNextPage) => {
+      base('Schedule').select({ view: 'Grid view' }).eachPage((records, fetchNextPage) => {
           setData(records.map((record) => record.fields));
           fetchNextPage();
-        });
-      }, []);
+      });
+  }, []);
 
-    return (
-
-<div className="columns">
-  
-    {data.map((item, index) => (
-      <div key={index} className="column is-3">{item.name}<br />
-      <img src={item.image?.[0]?.url || 'default_image_url'}  />
-      <br />{item.type}
-      </div>
-    ))}
-
-
-</div>
-
-    );
+  return (
+      <Columns multiline>
+          {data.map((item, index) => (
+              <Columns.Column key={index} size={3}>
+                  <div>
+                      {item.name}<br />
+                      <img src={item.image?.[0]?.url || 'default_image_url'} alt={item.name} />
+                      <br />{item.type}
+                  </div>
+              </Columns.Column>
+          ))}
+      </Columns>
+  );
 }
